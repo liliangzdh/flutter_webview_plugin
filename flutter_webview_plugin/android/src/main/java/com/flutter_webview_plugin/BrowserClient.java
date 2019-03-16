5ee11e690e8fcbb72b7a29f68147d45d6d78e950
liliang <1582880360@qq.com>
2019-03-16 19:11:30 +0800

    1.修改webview err err_unknown_url_scheme

--- flutter_webview_plugin/android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
+++ flutter_webview_plugin/android/src/main/java/com/flutter_webview_plugin/BrowserClient.java
@@ -48,4 +48,16 @@ public class BrowserClient extends WebViewClient {
         data.put("code", Integer.toString(errorResponse.getStatusCode()));
         FlutterWebviewPlugin.channel.invokeMethod("onHttpError", data);
     }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (url.startsWith("http:") || url.startsWith("https:")) {
+            view.loadUrl(url);
+            return false;
+        } else {
+//            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//            startActivity(intent);
+            return true;
+        }
+    }
 }
